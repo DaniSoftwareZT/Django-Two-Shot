c6a19493ed3fe745d89ad0fba55b777bd98b0da7
Daniel <rippedchili@gmail.com>
2022-12-02 14:43:26 -0400

    Feature 4 Complete

--- receipts/admin.py
+++ receipts/admin.py
@@ -1,3 +1,15 @@
 from django.contrib import admin
-
+from receipts.models import ExpenseCategory, Account, Receipt
 # Register your models here.
+
+
+class ExpenseCategoryAdmin(admin.ModelAdmin):
+    admin.site.register(ExpenseCategory)
+
+
+class AccountAdmin(admin.ModelAdmin):
+    admin.site.register(Account)
+
+
+class ReceiptAdmin(admin.ModelAdmin):
+    admin.site.register(Receipt)
