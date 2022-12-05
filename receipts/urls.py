4dcc353f75c388dc60bedf20d4ce737a1b9d415a
Daniel <rippedchili@gmail.com>
2022-12-05 18:44:57 -0400

    Feature 12 Complete

--- receipts/urls.py
+++ receipts/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from receipts.views import receipt_list, create_receipt_view
+from receipts.views import receipt_list, create_receipt_view, category_list, account_list
 
 
 urlpatterns = [
     path("", receipt_list, name="home"),
     path("create/", create_receipt_view, name="create_receipt"),
+    path("categories/", category_list, name="category_list"),
+    path("accounts/", account_list, name="account_list"),
 ]
 #path("urlpath", view_function_name, name=url name)
--- receipts/views.py
+++ receipts/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from .models import Receipt
+from .models import Receipt, Account, ExpenseCategory
 from django.contrib.auth.decorators import login_required
 from .forms import CreateViewForm
 
@@ -7,15 +7,30 @@ from .forms import CreateViewForm
 
 
 @login_required
-def receipt_list(request):
-    receipt_list = Receipt.objects.filter(purchaser = request.user)
-    context = {
-        "receipt_list": receipt_list #connected to line 8
-    }
-    #use the context dictionary key "" to access receipt_list in templates
-    return render(request, "receipts/list.html", context) #pass context to render function
+def category_list(request):
+    category_list = ExpenseCategory.objects.filter(owner=request.user)
+    context = {"category_list": category_list}
+    return render(request, "expenses/list.html", context)
 
-#this @ = you have to be logged in to access it
+
+@login_required
+def account_list(request):
+    account_list = Account.objects.filter(owner=request.user)
+    context = {"account_list": account_list}
+    return render(request, "accounts/list.html", context)
+
+
+@login_required
+def receipt_list(request):
+    receipt_list = Receipt.objects.filter(purchaser=request.user)
+    context = {"receipt_list": receipt_list}  # connected to line 8
+    # use the context dictionary key "" to access receipt_list in templates
+    return render(
+        request, "receipts/list.html", context
+    )  # pass context to render function
+
+
+# this @ = you have to be logged in to access it
 @login_required
 def create_receipt_view(request):
     if request.method == "POST":
@@ -28,8 +43,6 @@ def create_receipt_view(request):
     else:
         form = CreateViewForm()
 
-    context = {
-        "form": form
-}
+    context = {"form": form}
 
     return render(request, "receipts/create.html", context)
